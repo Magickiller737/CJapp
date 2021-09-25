2e03bf9d59041c4264c15563a68df02e08a64f7a
Magickiller737 <qq3282680863@gmail.com>
2021-09-25 18:40:49 +0800

    Signed-off-by: Magickiller737 <qq3282680863@gmail.com>

---
+++ app/src/main/java/ShuJuKu/TieZiLianJie.java
@@ -0,0 +1,40 @@
+package ShuJuKu;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TieZiLianJie {
+    public Connection con=null;
+    public PreparedStatement ps;
+    public ResultSet re;
+
+
+    public void openDB() throws ClassNotFoundException {
+
+        Class.forName("com.mysql.jdbc.Driver");//加载驱动
+        try {
+            con= DriverManager.getConnection(
+                    "********",
+                    "********",
+                    "********");// 输入连接参数
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void closeResoure() throws SQLException {
+        if(con!=null){
+            con.close();
+        }
+        if(re!=null){
+            re.close();
+        }
+        if(ps!=null){
+            ps.close();
+        }
+    }
+}
